2b497cbe241413154598a130e0b7bdc806aed9c0
Mark Larsen <mark.aaju@gmail.com>
2017-03-05 18:05:16 +0800

    More linting options

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,10 @@ module.exports = {
         "react",
         "jsx-a11y",
         "import"
-    ]
+    ],
+    "rules": {
+      "semi": ["error", "never"],
+      "class-method-use-this": 0,
+      "react/jsx-filename-extension": 0,
+    }
 };
